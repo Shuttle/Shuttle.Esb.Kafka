d30b5272131ddb81dc45fa00556bb086e994d7cf
Eben <me@ebenroux.co.za>
2022-07-30 18:16:16 +0200

    Update KafkaFixture.cs

--- Shuttle.Esb.Kafka.Tests/KafkaFixture.cs
+++ Shuttle.Esb.Kafka.Tests/KafkaFixture.cs
@@ -1,28 +1,35 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Shuttle.Core.Pipelines;
 using Shuttle.Esb.Logging;
 
 namespace Shuttle.Esb.Kafka.Tests
 {
     public class KafkaFixture
     {
-        public static IServiceCollection GetServiceCollection(bool useCancellationToken = false)
+        public static IServiceCollection GetServiceCollection(bool useCancellationToken = false, bool log = false)
         {
             var services = new ServiceCollection();
 
             services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
 
-            services.AddServiceBusLogging(builder =>
+            if (log)
             {
-                builder.Options.AddPipelineEventType<OnGetMessage>();
-            });
+                services.AddServiceBusLogging(builder =>
+                {
+                    builder.Options.AddPipelineEventType<OnAbortPipeline>();
+                    builder.Options.AddPipelineEventType<OnPipelineStarting>();
+                    builder.Options.AddPipelineEventType<OnPipelineException>();
+                    builder.Options.AddPipelineEventType<OnGetMessage>();
+                });
 
-            services.AddLogging(builder =>
-            {
-                builder.SetMinimumLevel(LogLevel.Trace);
-                builder.AddConsole();
-            });
+                services.AddLogging(builder =>
+                {
+                    builder.SetMinimumLevel(LogLevel.Trace);
+                    builder.AddConsole();
+                });
+            }
 
             services.AddKafka(builder =>
             {
