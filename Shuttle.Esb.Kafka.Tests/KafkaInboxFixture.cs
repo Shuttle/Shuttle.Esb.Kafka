c9015310aa7e3060bb1e6f16e2f49dcd182e87c0
Eben <me@ebenroux.co.za>
2024-02-04 12:22:18 +0200

    Update KafkaInboxFixture.cs

--- Shuttle.Esb.Kafka.Tests/KafkaInboxFixture.cs
+++ Shuttle.Esb.Kafka.Tests/KafkaInboxFixture.cs
@@ -14,13 +14,6 @@ namespace Shuttle.Esb.Kafka.Tests
             await TestInboxError(KafkaFixture.GetServiceCollection(), "kafka://local/{0}", hasErrorQueue, isTransactionalEndpoint);
         }
 
-        [TestCase(250, false)]
-        [TestCase(250, true)]
-        public async Task Should_be_able_to_process_messages_concurrently(int msToComplete, bool isTransactionalEndpoint)
-        {
-            await TestInboxConcurrency(KafkaFixture.GetServiceCollection(), "kafka://local/{0}", msToComplete, isTransactionalEndpoint);
-        }
-
         [TestCase(100, true)]
         [TestCase(100, false)]
         public async Task Should_be_able_to_process_queue_timeously(int count, bool isTransactionalEndpoint)
